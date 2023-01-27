56f45f990abd12bfda5ea09d0126d6b89b4d77e8
Ivan <ivanlsowerby@gmail.com>
2023-01-27 19:25:42 +0000

    Release 0.0.7

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -1,20 +1,29 @@
 import * as React from "react";
 
-const title = {
-  greeting: "Hey",
-  name: "React"
-};
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org",
+    objectId: 0
+  }, {
+    title: "Redux",
+    url: "https://redux.js.org",
+    objectId: 1
+  }
+]
 
 function App() {
-  return (
-    <div>
-      <h1>{title.greeting}, {title.name}</h1>
-      <label>
-        <label htmlFor="search">Search: </label>
-        <input id="search" type="text"></input>
-      </label>
-    </div>
-  );
+  return list.map(obj => {
+    return (
+      <table>
+        <tr>
+          <th>{obj.title}</th>
+          <td>{obj.url}</td>
+          <td>{obj.objectId}</td>
+        </tr>
+      </table>
+    );
+  })
 }
 
 export default App;
