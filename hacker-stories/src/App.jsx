9b139b7c77d76047e847558e5eed61c792854794
Ivan <ivanlsowerby@gmail.com>
2023-01-27 17:27:18 +0000

    Release 0.0.5

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -6,6 +6,10 @@ function App() {
   return (
     <div>
       <h1>{title}</h1>
+      <label>
+        <label htmlFor="search">Search: </label>
+        <input id="search" type="text"></input>
+      </label>
     </div>
   );
 }
